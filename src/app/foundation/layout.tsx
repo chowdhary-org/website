1b520cdc416403632ee6ca9944cda1aa2fb9e97d
Anand Chowdhary <mail@anandchowdhary.com>
2024-04-25 13:51:50 +0200

    :wrench: Add metadata and viewport tags

--- src/app/foundation/layout.tsx
+++ src/app/foundation/layout.tsx
@@ -1,5 +1,9 @@
 import { Layout } from '@/app/holdings/components/Layout'
-import { type Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
+
+export const viewport: Viewport = {
+  themeColor: '#0ca5e9',
+}
 
 export const metadata: Metadata = {
   title: {
@@ -8,6 +12,24 @@ export const metadata: Metadata = {
   },
   description:
     'At Chowdhary.org, we are a nonprofit harnessing the power of technology to open doors to a better future.',
+  icons: [
+    { rel: 'apple-touch-icon', sizes: '180x180', url: '/apple-touch-icon.png' },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/favicon-16x16.png',
+    },
+    { rel: 'manifest', url: '/site.webmanifest' },
+    { rel: 'mask-icon', color: '#15b8a6', url: '/safari-pinned-tab.svg' },
+  ],
+  other: { 'msapplication-TileColor': '#15b8a6' },
 }
 
 export default function MainLayout({
--- src/app/holdings/layout.tsx
+++ src/app/holdings/layout.tsx
@@ -1,5 +1,9 @@
 import { Layout } from '@/app/holdings/components/Layout'
-import { type Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
+
+export const viewport: Viewport = {
+  themeColor: '#0ca5e9',
+}
 
 export const metadata: Metadata = {
   title: {
@@ -8,6 +12,24 @@ export const metadata: Metadata = {
   },
   description:
     'At Chowdhary.co, we make early-stage investments to accelerate technological progress and drive social impact.',
+  icons: [
+    { rel: 'apple-touch-icon', sizes: '180x180', url: '/apple-touch-icon.png' },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/favicon-16x16.png',
+    },
+    { rel: 'manifest', url: '/site.webmanifest' },
+    { rel: 'mask-icon', color: '#0ca5e9', url: '/safari-pinned-tab.svg' },
+  ],
+  other: { 'msapplication-TileColor': '#0ca5e9' },
 }
 
 export default function MainLayout({
