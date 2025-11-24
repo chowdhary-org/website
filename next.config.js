79463fd61f9b77dd644657c1e8dcd3bb1fb594e2
Anand Chowdhary <mail@anandchowdhary.com>
2025-11-24 11:20:15 +0100

    :wrench: Add redirect for Gita Book 2025

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  async redirects() {
+    return [
+      {
+        source: '/gita-book-2025',
+        destination: 'https://chowdhary-org.github.io/bhagavadgita-ka-saral-paath/'
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
